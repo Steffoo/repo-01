427f1a109f10aa64cd6a91d4bfc0317c52233e60
FHefner <mail@felixhefner.de>
2017-11-23 23:41:10 +0100

    Added make_aci.sh script which creates an aci-file (rkt-Image) for tomcat. Moved scripts to new "scripts/"-dir.

--- kill_tomcat.sh
+++
@@ -1,8 +0,0 @@
-cmd="java -jar tomcat-6.0.5-$1.jar"
-process=$(ps aux | grep "$cmd" | head -n1 | cut -d " " -f 4)
-echo $process
-if ps -p $process > /dev/null
-then
-	kill -9 $process
-fi
-
---
+++ scripts/make_aci.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$1" == "CI" ]; then
+	workdir=/var/lib/jenkins
+else
+	workdir=/var/lib/jenkins/production
+fi
+cd $workdir
+user=$(whoami)
+acifile=tomcat.aci
+rm -f tomcat.aci || true
+sudo acbuild begin
+sudo acbuild dependency add quay.io/extendedmind/openjdk:openjdk-8u121
+sudo acbuild port add tomcat-http tcp 8081
+sudo acbuild copy $workdir/tomcat-6.0.5-$1.jar /tmp/tomcat.jar
+sudo acbuild copy-to-dir $workdir/webapps/* /webapps
+sudo acbuild copy-to-dir $workdir/conf/* /conf
+sudo acbuild set-exec -- nohup java -jar /tmp/tomcat.jar
+sudo acbuild set-name apache.org/tomcat
+sudo acbuild annotation add authors "Max Jando<max.jando1@stud.hs-mannheim.de>, Felix Hefner<mail@felixhefner.de>, Severin Kohler<severin.kohler@stud.hs-mannheim.de"
+sudo acbuild write $acifile
+sudo acbuild end
+sudo chown $user $acifile
